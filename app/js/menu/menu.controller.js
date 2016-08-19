b0795e3d9cad35c888e670959891c7963d22d21f
WoutervanGeel <w.vangeel@student.avans.nl>
2016-08-19 17:28:56 +0200

    fixed the menu

--- app/js/menu/menu.controller.js
+++ app/js/menu/menu.controller.js
@@ -1,29 +1,35 @@
 module.exports = function ($scope, AuthenticationService, DashBoardService, $mdToast, $mdDialog, $window, $state) {
-    this.self = this;
-
-    this.self.minPlayers = 1;
-    this.self.maxPlayers = 32;
-    this.self.players = [];
-    this.self.gameTemplates = ["Snake", "Ox", "Dragon", "Shanghai", "Monkey", "Ram", "Rooster"];
+    var self = this;
     
-    for (i = this.self.minPlayers; i < this.self.maxPlayers + 1; i++) {
-        this.self.players.push(i);
+    self.players = [];
+    
+    // restricties zetten
+    self.minPlayers = 1;
+    self.maxPlayers = 32;
+    self.gameTemplates = ["Dragon", "Monkey", "Ox", "Ram", "Rooster", "Shanghai", "Snake"];
+    
+    // vullen van de players
+    for (i = self.minPlayers; i < self.maxPlayers + 1; i++) {
+        self.players.push(i);
     }
 
-    this.self.goToLogin = function () {
+    //externe login starten
+    self.goToLogin = function () {
         AuthenticationService.goToExternalLogin();
     };
 
-    this.self.logOut = function () {
-        console.log("Uitloggen");
+    //uitloggen
+    self.logOut = function () {
         AuthenticationService.logOut();
     };
 
-    this.self.goToDashboard = function () {
+    // verwijzen naar dashboard
+    self.goToDashboard = function () {
         $state.go('app.dashboard');
     };
 
-    this.self.showAddGame = function () {
+    //popup starten om game aan te maken
+    self.showAddGamePopup = function () {
         $mdDialog.show({
             templateUrl: 'views/dashboard/add_game.html',
             controller: 'MenuController as MenuC',
@@ -31,28 +37,37 @@ module.exports = function ($scope, AuthenticationService, DashBoardService, $mdT
             clickOutsideToClose: false
         });
     };
-    this.self.goToPref = function () {
+    
+    //popup starten om preferences aan te passen
+    self.goToPreferences = function () {
         $mdDialog.show({
             templateUrl: 'views/preference/preferences.html',
-            controller: 'PreferenceController as PrefC',
+            controller: 'PreferenceController as preferencesC',
             parent: angular.element(document.body),
             clickOutsideToClose: true
         });
     }
-    this.self.closeAddGame = function () {
+    
+    //stoppen van popup voor het maken van een game
+    self.closeAddGamePopup = function () {
         $mdDialog.hide();
     }
-    this.self.addGame = function () {
+    
+    // game aanmaken
+    self.addGame = function () {
+        //opgeven van de waardes
         DashBoardService.setGameSettings($scope.game.template, $scope.game.minPlayers, $scope.game.maxPlayers);
 
+        //aanvragen van een nieuwe game
         DashBoardService.addGame(function (result) {
             if (result.statusText == 'OK') {
-                console.log(result.data);
+                //gelukt
                 $mdToast.show($mdToast.simple().textContent('Nieuwe game aangemaakt'));
                 $mdDialog.hide();
                 $window.location.reload();
             }
             else {
+                //error
                 console.log(result);
             }
         });

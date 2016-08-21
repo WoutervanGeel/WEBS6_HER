f0d82cd7bcd05bb6796910db62f8a13fece5456d
WoutervanGeel <w.vangeel@student.avans.nl>
2016-08-21 23:00:33 +0200

    filter gefixed en test ervoor gemaakt

--- test/gameState.filter.spec.js
+++ test/gameState.filter.spec.js
@@ -56,8 +56,9 @@ describe('GameState filter test', function () {
 
     beforeEach(module('webs6'));
 
-    beforeEach(inject(function (_$filter_) {
+    beforeEach(inject(function (_$filter_, _$rootScope_) {
         $filter = _$filter_;
+        $rootScope = _$rootScope_.$new();
     }));
 
     it('should return 3 for number of open games in the list', function () {
@@ -66,23 +67,33 @@ describe('GameState filter test', function () {
         var result = $filter('gameState')(tempGames, "open");
         expect(result).to.be.length(3);
     });
+    
+    it('should return 1 for number of playing games in the list', function () {
+        var tempGames = games;
 
-    // it('should return false when id is in te list', function () {
-    //     var players = []
+        var result = $filter('gameState')(tempGames, "playing");
+        expect(result).to.be.length(1);
+    });
+    
+    it('should return 2 for number of finished games in the list', function () {
+        var tempGames = games;
 
-    //     players.push(
-    //         {
-    //             _id: '1'
-    //         },
-    //         {
-    //             _id: 'id2'
-    //         },
-    //         {
-    //             _id: '3'
-    //         });
+        var result = $filter('gameState')(tempGames, "finished");
+        expect(result).to.be.length(2);
+    });
+    
+    it('should return 6 for all games in the list', function () {
+        var tempGames = games;
 
-    //     var result = $filter('spectate')(players, currentPlayer);
-    //     console.log(result);
-    //     expect(result).to.be.false;
-    // });
+        var result = $filter('gameState')(tempGames, "");
+        expect(result).to.be.length(6);
+    });
+    
+    it('should return 2 for number of games created by the user in the list', function () {
+        var tempGames = games;
+
+        var result = $filter('gameState')(tempGames, currentPlayer);
+        expect(result).to.be.length(2);
+    });
+
 });
